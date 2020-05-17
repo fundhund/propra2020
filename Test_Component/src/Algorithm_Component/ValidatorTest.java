3f8e6bf4405d9f546c6b870bfb49288d19b7fe1e
fundhund <marius.mielke@googlemail.com>
2020-05-17 14:29:37 +0200

    More tests for Validator class

--- Test_Component/src/Algorithm_Component/ValidatorTest.java
+++ Test_Component/src/Algorithm_Component/ValidatorTest.java
@@ -84,4 +84,126 @@ public class ValidatorTest {
 		// Assert
 		assertFalse("Validator returned incorrect result.", isValid);
 	}
+	
+	@Test
+	public void Validator_validate_returnsTrueForValidArcShapedRoom() throws IncorrectShapeException {
+		
+		// Arrange
+		String id = "id";
+		List<Point2D.Float> corners = TestHelper.getCornersForArcShape();
+		List<Point2D.Float> lamps = new ArrayList<>();
+		lamps.add(new Point2D.Float(0.5f, 1.5f));
+		lamps.add(new Point2D.Float(2.5f, 1.5f));
+		Room room = new Room(id, corners, lamps);
+		
+		Validator validator = new Validator(room);
+		
+		// Act
+		boolean isValid = validator.validate();
+		
+		// Assert
+		assertTrue("Validator returned incorrect result.", isValid);
+	}
+	
+	@Test
+	public void Validator_validate_returnsFalseForOneLampInArcShapedRoom() throws IncorrectShapeException {
+		
+		// Arrange
+		String id = "id";
+		List<Point2D.Float> corners = TestHelper.getCornersForArcShape();
+		List<Point2D.Float> lamps = new ArrayList<>();
+		lamps.add(new Point2D.Float(2.5f, 1.5f));
+		Room room = new Room(id, corners, lamps);
+		
+		Validator validator = new Validator(room);
+		
+		// Act
+		boolean isValid = validator.validate();
+		
+		// Assert
+		assertFalse("Validator returned incorrect result.", isValid);
+	}
+	
+	@Test
+	public void Validator_validate_returnsTrueForValidArcShapedRoomWithMoreLampsThanNecessary() throws IncorrectShapeException {
+		
+		// Arrange
+		String id = "id";
+		List<Point2D.Float> corners = TestHelper.getCornersForArcShape();
+		List<Point2D.Float> lamps = new ArrayList<>();
+		lamps.add(new Point2D.Float(0.5f, 1.5f));
+		lamps.add(new Point2D.Float(2.5f, 1.5f));
+		lamps.add(new Point2D.Float(1.5f, 1.5f));
+		Room room = new Room(id, corners, lamps);
+		
+		Validator validator = new Validator(room);
+		
+		// Act
+		boolean isValid = validator.validate();
+		
+		// Assert
+		assertTrue("Validator returned incorrect result.", isValid);
+	}
+	
+	@Test
+	public void Validator_validate_returnsTrueForValidOpenRingShapedRoom() throws IncorrectShapeException {
+		
+		// Arrange
+		String id = "id";
+		List<Point2D.Float> corners = TestHelper.getCornersForOpenRingShape();
+		List<Point2D.Float> lamps = new ArrayList<>();
+		lamps.add(new Point2D.Float(1.5f, 1.5f));
+		lamps.add(new Point2D.Float(7.5f, 1.5f));
+		lamps.add(new Point2D.Float(1.5f, 7.5f));
+		Room room = new Room(id, corners, lamps);
+		
+		Validator validator = new Validator(room);
+		
+		// Act
+		boolean isValid = validator.validate();
+		
+		// Assert
+		assertTrue("Validator returned incorrect result.", isValid);
+	}
+	
+	@Test
+	public void Validator_validate_returnsTrueForValidOpenRingShapedRoomWithAlternativeLamps() throws IncorrectShapeException {
+		
+		// Arrange
+		String id = "id";
+		List<Point2D.Float> corners = TestHelper.getCornersForOpenRingShape();
+		List<Point2D.Float> lamps = new ArrayList<>();
+		lamps.add(new Point2D.Float(1.5f, 1.5f));
+		lamps.add(new Point2D.Float(7.5f, 1.5f));
+		lamps.add(new Point2D.Float(7.5f, 7.5f));
+		Room room = new Room(id, corners, lamps);
+		
+		Validator validator = new Validator(room);
+		
+		// Act
+		boolean isValid = validator.validate();
+		
+		// Assert
+		assertTrue("Validator returned incorrect result.", isValid);
+	}
+	
+	@Test
+	public void Validator_validate_returnsFalseForOpenRingShapedRoomWithTooFewLamps() throws IncorrectShapeException {
+		
+		// Arrange
+		String id = "id";
+		List<Point2D.Float> corners = TestHelper.getCornersForOpenRingShape();
+		List<Point2D.Float> lamps = new ArrayList<>();
+		lamps.add(new Point2D.Float(1.5f, 1.5f));
+		lamps.add(new Point2D.Float(7.5f, 1.5f));
+		Room room = new Room(id, corners, lamps);
+		
+		Validator validator = new Validator(room);
+		
+		// Act
+		boolean isValid = validator.validate();
+		
+		// Assert
+		assertFalse("Validator returned incorrect result.", isValid);
+	}
 }
