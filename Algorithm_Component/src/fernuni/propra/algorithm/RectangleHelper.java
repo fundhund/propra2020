5e9d3832a35b649d14d98b1c5713f2860a01b78d
fundhund <marius.mielke@googlemail.com>
2020-06-02 10:13:08 +0200

    refactored rectangleHelper, javadocs

--- Algorithm_Component/src/fernuni/propra/algorithm/RectangleHelper.java
+++ Algorithm_Component/src/fernuni/propra/algorithm/RectangleHelper.java
@@ -2,30 +2,41 @@ package fernuni.propra.algorithm;
 
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
+import java.awt.geom.Rectangle2D.Float;
 
 public class RectangleHelper {
 	
-	// Modified Rectangle source code
+	/**
+	 * Computes the intersection of two rectangles.
+	 * 
+	 * @param r1 first rectangle
+	 * @param r2 second rectangle
+	 * @return the largest rectangle contained in both rectangles
+	 */
 	public static Rectangle2D.Float getIntersection(Rectangle2D.Float r1, Rectangle2D.Float r2) {
 		float r1x1 = r1.x;
+		float r1x2 = r1x1 + r1.width;
     float r1y1 = r1.y;
+    float r1y2 = r1y1 + r1.height;
     float r2x1 = r2.x;
+    float r2x2 = r2x1 + r2.width;
     float r2y1 = r2.y;
-    float r1x2 = r1x1; r1x2 += r1.width;
-    float r1y2 = r1y1; r1y2 += r1.height;
-    float r2x2 = r2x1; r2x2 += r2.width;
-    float r2y2 = r2y1; r2y2 += r2.height;
-    if (r1x1 < r2x1) r1x1 = r2x1;
-    if (r1y1 < r2y1) r1y1 = r2y1;
-    if (r1x2 > r2x2) r1x2 = r2x2;
-    if (r1y2 > r2y2) r1y2 = r2y2;
-    r1x2 -= r1x1;
-    r1y2 -= r1y1;
-    if (r1x2 < Float.MIN_VALUE) r1x2 = Float.MIN_VALUE;
-    if (r1y2 < Float.MIN_VALUE) r1y2 = Float.MIN_VALUE;
-    return new Rectangle2D.Float(r1x1, r1y1, (float) r1x2, (float) r1y2);
+    float r2y2 = r2y1 + r2.height;
+    
+    float ix = r1x1 > r2x1 ? r1x1 : r2x1;
+    float iy = r1y1 > r2y1 ? r1y1 : r2y1;
+    float iw = (r1x2 < r2x2 ? r1x2 : r2x2) - ix;
+    float ih = (r1y2 < r2y2 ? r1y2 : r2y2) - iy;
+    
+    return new Rectangle2D.Float(ix, iy, iw, ih);
 	}
 	
+	/**
+	 * Computes the center of a rectangle
+	 * 
+	 * @param rectangle a rectangle
+	 * @return the center of given rectangle
+	 */
 	public static Point2D.Float getCenter(Rectangle2D.Float rectangle) {
 		float x = (rectangle.x + rectangle.width) / 2;
 		float y = (rectangle.y + rectangle.height) / 2;
